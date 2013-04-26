8ba4f6b574f8c996606dac65d13e05acbc117d6b
Simon Elvery <simon@elvery.net>
2013-04-26 10:51:03 +1000

    Code against a theme called 'default'
    This is temporary until the styleguide is setup to use whichever theme is active in SS.

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 define({
 	// This css path is used as a default by the Kalei project it self.
-	css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) +  '/themes/silverstripe-boilerplate-theme/styleguide/style.css',
+	css_path: window.location.protocol + '//' + window.location.hostname + (window.location.port === '' ? '' : ':'+ window.location.port) +  '/themes/default/styleguide/style.css',
 	
 	// You can optionally set configuration for marked.js
 	marked_options: {
